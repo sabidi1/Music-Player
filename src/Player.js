c4c12a0182d7f1cfcb7c96d1b5e451fb78fc9e82
Maxim Ruzmaykin <mruzmaykin@gmail.com>
2019-04-04 17:09:12 -0700

    working play button

--- src/Player.js
+++ src/Player.js
@@ -4,25 +4,30 @@ import './Player.css';
 class Player extends Component {
     constructor() {
         super();
+        this.playing = true;
         this.state = {
             file: 'http://localhost:8000/Imagine%20Dragons%20-%20Natural.mp3'
         };
     }
     play() {
-        this.refs.player.play();
+        if(this.playing == true)
+            this.refs.player.play();
+        else
+            this.refs.player.pause();
+        this.playing = !this.playing;
     }
     render() {
         return (
             <div className="player">
                 <div className="controls">
-                    <a href="javascript:void();">
+                    <a>
                         <i className="fas fa-fast-backward"></i>
                     </a>
-                    <a onClick={this.play.bind(this)} href="javascript:void();">
+                    <a onClick={this.play.bind(this)}>
                         <i className="fas fa-play"></i>
                     </a>
 
-                    <a href="javascript:void();">
+                    <a>
                         <i className="fas fa-fast-forward"></i>
                     </a>
                 </div>
